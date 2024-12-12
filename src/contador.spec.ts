4ffb37d0acf05d9e4095cb9412c8c334e6287310
Fernando Dodino <fernando.dodino@gmail.com>
2024-12-11 23:58:26 -0300

    Standardize projects

--- src/contador.spec.ts
+++ src/contador.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest'
-import { fireEvent, render, screen } from '@testing-library/svelte'
+import { render, screen } from '@testing-library/svelte'
 import userEvent from '@testing-library/user-event'
 
 import Conversor from './routes/+page.svelte'
@@ -7,36 +7,36 @@ import Conversor from './routes/+page.svelte'
 describe('Conversor', () => {
 
   it('should start with 0 miles & kilometers', () => {
-		render(Conversor)
+    render(Conversor)
 
-		const miles = screen.getByTestId('millas') as HTMLInputElement
-		expect(miles.value).to.equal('0')
+    const miles = screen.getByTestId('millas') as HTMLInputElement
+    expect(miles.value).to.equal('0')
 
-		const kilometers = screen.getByTestId('kilometers')
-		expect(+kilometers.innerHTML).to.equal(0)
-	})
+    const kilometers = screen.getByTestId('kilometers')
+    expect(+kilometers.innerHTML).to.equal(0)
+  })
 
   it('should convert valid miles to kilometers to locale es', async () => {
-		render(Conversor)
+    render(Conversor)
 
-		const miles = screen.getByTestId('millas') as HTMLInputElement
+    const miles = screen.getByTestId('millas') as HTMLInputElement
     await userEvent.type(miles, '100')
-		const kilometers = screen.getByTestId('kilometers')
-		expect(kilometers.innerHTML).to.equal('160,934')
-	})
+    const kilometers = screen.getByTestId('kilometers')
+    expect(kilometers.innerHTML).to.equal('160,934')
+  })
 
   it('reset puts miles value back to zero', async () => {
-		render(Conversor)
+    render(Conversor)
 
-		const miles = screen.getByTestId('millas') as HTMLInputElement
+    const miles = screen.getByTestId('millas') as HTMLInputElement
     await userEvent.type(miles, '100')
     const user = userEvent.setup()
     
     await user.click(screen.getByTestId('reset'))
 
     expect(miles.value).to.equal('0')
-		const kilometers = screen.getByTestId('kilometers')
-		expect(kilometers.innerHTML).to.equal('0')
-	})
+    const kilometers = screen.getByTestId('kilometers')
+    expect(kilometers.innerHTML).to.equal('0')
+  })
 
 })
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,28 @@
-<script lang="ts">
-	import './styles.css'
-	import { convertMilesToKms } from '$lib'
+<script lang='ts'>
+  import './styles.css'
+  import { convertMilesToKms } from '$lib'
 
-  const formattedNumber = (value: number) => value.toLocaleString('es');
+  const formattedNumber = (value: number) => value.toLocaleString('es')
 
-	let miles = $state(0)
-	let kilometers = $derived(formattedNumber(convertMilesToKms(miles)))
+  let miles = $state(0)
+  let kilometers = $derived(formattedNumber(convertMilesToKms(miles)))
 
-  const reset = () => { miles = 0 }
+  const reset = () => {
+    miles = 0
+  }
 </script>
 
-<div class="form">
+<div class='form'>
   <h1>Conversor millas a kms</h1>
-  <div class="row">
-    <label for="millas" class="label">Millas</label>
+  <div class='row'>
+    <label for='millas' class='label'>Millas</label>
     <input type='number' data-testid='millas' bind:value={miles} />
   </div>
-  <div class="row">
-    <label for="kilometros" class="label">Kilómetros</label>
-    <span data-testid="kilometers">{kilometers}</span>
+  <div class='row'>
+    <label for='kilometros' class='label'>Kilómetros</label>
+    <span data-testid='kilometers'>{kilometers}</span>
   </div>
-  <div class="row">
-    <button class="button secondary" data-testid="reset" onclick={reset}>Resetear</button>
+  <div class='row'>
+    <button class='button secondary' data-testid='reset' onclick={reset}>Resetear</button>
   </div>
 </div>
